e28f6e4ddb9b4fa358a57ea03b19633d86b1250d
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-11-04 19:27:50 +0530

    remove files and folders

--- src/components/getFaviconUrl.js
+++
@@ -1,9 +0,0 @@
-export const getFaviconUrl = (url) => {
-  try {
-    const parsedUrl = new URL(url);
-    return `${parsedUrl.origin}/favicon.ico`;
-  } catch (error) {
-    console.error("Invalid URL", error);
-    return "/path/to/default-icon.png";
-  }
-};
