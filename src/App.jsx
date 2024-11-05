bf67a34b6b2b72a9d0e1677e254803c8b93c9624
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-11-05 23:41:57 +0530

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PasswordManager from './PasswordManager';
+import PasswordDetails from './PasswordDetails';
 
 const App = () => {
   return (
-    <></>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<PasswordManager />} />
+          <Route path="/details/:id" element={<PasswordDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
